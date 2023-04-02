04b19e8ac4e31f55b5d184992fba4acca1d1bf56
bulk <harryhamlin@gmail.com>
2023-04-02 09:13:17 -0700

    added reaction controllers

--- controllers/reactionControllers.js
+++ controllers/reactionControllers.js
@@ -1,6 +1,38 @@
-const Reaction = require('../models/Reaction');
+const Thought = require('../models/Thought')
 
 module.exports = {
-    createReaction(req, res) {},
-    deleteReaction(req, res) {}
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { new: true }
+        )
+            .then((thought) => {
+                !thought
+                    ? res.status(404).json({
+                        message: 'no thought found'
+                    })
+                    : res.status(200).json({
+                        message: 'reaction added'
+                    })
+            })
+            .catch((err) => res.status(500).json(err))
+    },
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.body.reactionId } } },
+            { new: true }
+        )
+            .then((thought) => {
+                !thought
+                    ? res.status(404).json({
+                        message: 'no thought found'
+                    })
+                    : res.status(200).json({
+                        message: 'reaction removed'
+                    })
+            })
+            .catch((err) => res.status(500).json(err))
+    }
 }
